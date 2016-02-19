73822ecec3ca2879754250602157c8361f308b50
Jordan Goulder <jordan.goulder@gmail.com>
2016-02-19 18:40:45 -0500

    Add resorce endpoints

--- settings.py
+++ settings.py
@@ -1,4 +1,55 @@
 
-DOMAIN = {}
+DOMAIN = {
+   'users' : {
+      'schema': {
+         'username': {
+            'type': 'string',
+            'required': True
+         },
+
+         'password': {
+            'type': 'string',
+            'required': True
+         },
+
+         'roles': {
+            'type': 'list',
+            'allowed': ["user", "admin"]
+         }
+      }
+   },
+
+   'members': {
+      'schema': {
+         'first_name': {
+            'type': 'string',
+            'required': True
+         },
+
+         'last_name': {
+            'type': 'string',
+            'required': True
+         },
+
+         'card_id': {
+            'type': 'string',
+            'required': True
+         }
+      },
+   },
+
+   'cards': {
+      'schema': {
+         'id': {
+            'type': 'string',
+            'required': True,
+            'unique': True
+         }
+      }
+   }
+}
+
+RESOURCE_METHODS = ['GET', 'POST']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
 DEBUG = True
