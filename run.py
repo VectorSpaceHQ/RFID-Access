0b6dabe9a63fb734c15e74d73a118b81494804ea
Jordan Goulder <jordan.goulder@gmail.com>
2016-02-19 22:31:55 -0500

    Add settings to run.py so that it will be easier to add auth to individual endpoints

--- run.py
+++ run.py
@@ -1,6 +1,69 @@
 import re
 from eve import Eve
 
+settings = {
+   'DOMAIN' : {
+      'users' : {
+         'schema': {
+            'username': {
+               'type': 'string',
+               'required': True
+            },
+
+            'password': {
+               'type': 'string',
+               'required': True
+            },
+
+            'roles': {
+               'type': 'list',
+               'allowed': ["user", "admin"]
+            }
+         }
+      },
+
+      'members': {
+         'schema': {
+            'first_name': {
+               'type': 'string',
+               'required': True
+            },
+
+            'last_name': {
+               'type': 'string',
+               'required': True
+            },
+
+            'card_id': {
+               'type': 'string',
+               'required': True
+            }
+         },
+      },
+
+      'cards': {
+         'schema': {
+            'id': {
+               'type': 'string',
+               'required': True,
+               'unique': True
+            }
+         },
+
+         'additional_lookup': {
+            'url': 'regex("[\w]+")',
+            'field': 'id'
+         }
+      }
+   },
+
+   'RESOURCE_METHODS' : ['GET', 'POST'],
+
+   'ITEM_METHODS' : ['GET', 'PATCH', 'PUT', 'DELETE'],
+
+   'DEBUG' : True
+}
+
 def post_get_callback(resource, request, payload):
 
    match = re.search('/cards/(\w+)$', request.path)
@@ -17,9 +80,8 @@ def post_get_callback(resource, request, payload):
          app.data.driver.db['cards'].insert({ 'id': card_id })
 
 
-
 if __name__ == '__main__':
-   app = Eve()
+   app = Eve(settings=settings)
 
    app.on_post_GET += post_get_callback
 
