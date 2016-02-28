5d3785f68363c9817deedf6f4a1f0eda09310d56
Jordan Goulder <jordan.goulder@gmail.com>
2016-02-28 16:08:28 -0500

    Add ability to refresh logs

--- static/js/LogController.js
+++ static/js/LogController.js
@@ -14,21 +14,32 @@
 
         loadLogs();
 
+        vm.refresh = function refresh() {
+            loadLogs();
+        };
+
         vm.isAdmin = function isAdmin() {
             return AuthService.isAdmin();
         };
 
         function loadLogs() {
+            vm.loading = true;
             var getLogs = LogService.getLogs();
 
             getLogs.$promise.then(
                 function () {
+                    vm.loading = false;
+
                     vm.logs = getLogs._items;
 
                     for (var i = 0; i < vm.logs.length; i++) {
                         var d = new Date(vm.logs[i]._created);
                         vm.logs[i].date = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
                     }
+                },
+
+                function () {
+                    vm.loading = false;
                 }
             );
         }
