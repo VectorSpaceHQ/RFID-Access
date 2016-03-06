73bb29bb32636a28c09661dd9e46ddcf624de883
Jordan Goulder <jordan.goulder@gmail.com>
2016-03-05 21:27:44 -0500

    Make client work with new layout

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,32 +1,103 @@
-gulp = require('gulp');
-concat = require('gulp-concat');
-minify = require('gulp-minify');
+gulp        = require('gulp');
+concat      = require('gulp-concat');
+minify      = require('gulp-minify');
+del         = require('del');
+runSequence = require('run-sequence');
 
-gulp.task('scripts', function (){
-    return gulp.src([
-        './static/js/app.js',
-        './static/js/IndexController.js',
-        './static/js/AuthHttpInterceptor.js',
-        './static/js/AuthService.js',
-        './static/js/Base64.js',
-        './static/js/LoginController.js',
-        './static/js/UserService.js',
-        './static/js/UserController.js',
-        './static/js/AddUserController.js',
-        './static/js/EditUserController.js',
-        './static/js/ResourceService.js',
-        './static/js/ResourceController.js',
-        './static/js/EditResourceController.js',
-        './static/js/AddResourceController.js',
-        './static/js/ChangeUserPasswordController.js',
-        './static/js/CardService.js',
-        './static/js/CardController.js',
-        './static/js/EditCardController.js',
-        './static/js/LogService.js',
-        './static/js/LogController.js',
-        './static/js/route.js'
-    ])
+gulp.task('build-clean', function () {
+    return del(['dist']);
+})
+
+gulp.task('build-js-scripts', function () {
+    return gulp
+        .src([
+            'src/js/app.js',
+            'src/js/IndexController.js',
+            'src/js/AuthHttpInterceptor.js',
+            'src/js/AuthService.js',
+            'src/js/Base64.js',
+            'src/js/LoginController.js',
+            'src/js/UserService.js',
+            'src/js/UserController.js',
+            'src/js/AddUserController.js',
+            'src/js/EditUserController.js',
+            'src/js/ResourceService.js',
+            'src/js/ResourceController.js',
+            'src/js/EditResourceController.js',
+            'src/js/AddResourceController.js',
+            'src/js/ChangeUserPasswordController.js',
+            'src/js/CardService.js',
+            'src/js/CardController.js',
+            'src/js/EditCardController.js',
+            'src/js/LogService.js',
+            'src/js/LogController.js',
+            'src/js/route.js'
+        ])
         .pipe(concat('rfid-app.js'))
         .pipe(minify())
-        .pipe(gulp.dest('./static/js'));
+        .pipe(gulp.dest('dist/js'));
 });
+
+gulp.task('build-js-libs', function () {
+    return gulp
+        .src([
+            'node_modules/angular/angular.min.js',
+            'node_modules/angular-route/angular-route.min.js',
+            'node_modules/angular-resource/angular-resource.min.js',
+            'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
+            'node_modules/angular-file-saver/dist/angular-file-saver.bundle.min.js',
+            'node_modules/ngstorage/ngStorage.min.js'
+        ])
+        .pipe(gulp.dest('dist/js/lib'));
+})
+
+gulp.task('build-html-index', function () {
+    return gulp
+        .src([
+            'src/index.html'
+        ])
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('build-html-views', function () {
+    return gulp
+        .src([
+            'src/views/*.html'
+        ])
+        .pipe(gulp.dest('dist/views'));
+})
+
+gulp.task('build-fonts', function () {
+    return gulp
+        .src([
+            'node_modules/bootstrap/fonts/*.*'
+        ])
+        .pipe(gulp.dest('dist/fonts'));
+})
+
+gulp.task('build-styles', function () {
+    return gulp
+        .src([
+            'src/styles/*.css',
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/angular-toastr/dist/angular-toastr.min.css',
+        ])
+        .pipe(gulp.dest('dist/styles'));
+})
+
+gulp.task('build', function (callback) {
+    return runSequence(
+        'build-clean',
+        [
+            'build-js-scripts',
+            'build-js-libs',
+            'build-html-index',
+            'build-html-views',
+            'build-fonts',
+            'build-styles'
+        ],
+        callback
+    );
+})
+
+gulp.task('default', ['build']);
