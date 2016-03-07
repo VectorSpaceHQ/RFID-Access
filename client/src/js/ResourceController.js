e6d4fb4f97456b5e5c35e643fadb093c54964681
Jordan Goulder <jordan.goulder@gmail.com>
2016-03-06 21:39:22 -0500

    Handle resource remove better

--- client/src/js/ResourceController.js
+++ client/src/js/ResourceController.js
@@ -18,28 +18,33 @@
             loadResources();
         };
 
-        vm.removeResource = function removeResource(id, etag) {
-            var removeResource = ResourceService.removeResource(id, etag);
+        vm.removeResource = function removeResource(resource) {
+            if (!resource.removing) {
+                resource.removing = true;
+                var removeResource = ResourceService.removeResource(resource.id, resource._etag);
 
-            removeResource.$promise.then(
-                function () {
-                    toastr.success('Resource successfully removed!');
-                    loadResources();
-                },
-                function (rejection) {
-                    var message = 'Unable to remove resource at this time.';
+                removeResource.$promise.then(
+                    function () {
+                        toastr.success('Resource successfully removed!');
+                        loadResources();
+                    },
+                    function (rejection) {
+                        resource.removing = false;
 
-                    if (rejection.status == 404) {
-                        message = 'This resource no longer exists.'
-                    } else if (rejection.status == 412) {
-                        message = 'This resource has changed since it was loaded.';
+                        var message = 'Unable to remove resource at this time.';
+
+                        if (rejection.status == 404) {
+                            message = 'This resource no longer exists.'
+                        } else if (rejection.status == 412) {
+                            message = 'This resource has changed since it was loaded.';
+                        }
+
+                        toastr.error(message, 'Error Removing Resource');
+
+                        loadResources();
                     }
-
-                    toastr.error(message, 'Error Removing Resource');
-
-                    loadResources();
-                }
-            )
+                )
+            }
         };
 
         vm.isAdmin = function isAdmin() {
