526522f1a73ebe8a39ff326e07f091b59ba51604
Adam Spontarelli <adam@vector-space.org>
2025-04-30 14:25:49 -0400

    adding framework for keypad. able to decode keypresses

--- reader/MFRC522-python/read_wiegand.py
+++ reader/MFRC522-python/read_wiegand.py
@@ -178,7 +178,126 @@ class decoder():
                 time.sleep(0.5)
                 GPIO.output(REAR_DOOR_SPKR_PIN, GPIO.HIGH)
 
-    
+class KeypadDecoder():
+    def __init__(self, gpio_0, gpio_1, location=""):
+        self.data = 0
+        self.bit_count = 0
+        self.bit_time = 0
+        self.location = location
+        self.relay_pin = 1
+        self.led_pin = 1
+        self.spkr_pin = 1
+        self.MAX_BITS = 32 # 8 bits per keypress
+        self.last_scantime = datetime.now()
+        
+        GPIO.setup(gpio_0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(gpio_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        GPIO.add_event_detect(gpio_0, GPIO.FALLING, callback=lambda x: self.getData0())
+        GPIO.add_event_detect(gpio_1, GPIO.FALLING, callback=lambda x: self.getData1())
+
+    def getData0(self):
+        self.bit_time = time.time_ns()
+        if self.bit_count < self.MAX_BITS:
+            self.bit_count += 1
+            self.data = self.data << 1
+        else:
+            self.process_scan()
+
+    def getData1(self):
+        self.bit_time = time.time_ns()
+        if self.bit_count < self.MAX_BITS:
+            self.bit_count += 1
+            self.data = self.data << 1
+            self.data = self.data | 1
+        else:
+            self.process_scan()
+
+    def get_pending_bit_count(self):
+        """
+        if time since last bit reading greater than READER_TIMEOUT or 1 second,
+        then accept the bit_count.
+        """
+        delta_sec = time.time() - (self.bit_time / 10**9)
+        delta_nsec = time.time_ns() - self.bit_time
+        if (delta_sec > 1 or delta_nsec > READER_TIMEOUT):
+            # print("bit count:", self.bit_count)
+            # print(delta_sec, delta_nsec)
+            return self.bit_count
+        return 0
+
+    def read_data(self):
+        # if self.get_pending_bit_count() > 0:
+        bit_count = self.bit_count
+        data = self.data
+        self.reset()
+        return data
+
+    def decode_data(self, data):
+        # keypad: each keypress is 8bit?
+        keypad_dict = {"11100001":"1",
+                       "11010010":"2",
+                       "11000011":"3",
+                       "10110100":"4",
+                       "10100101":"5",
+                       "10010110":"6",
+                       "10000111":"7",
+                       "01111000":"8",
+                       "01101001":"9",
+                       "11110000":"0",
+                       "01011010":"*",
+                       "01001011":"#"}
+        try:
+            data = str(data)
+            keycode = ""
+            for i in range(0, len(data), 8):
+                key = data[i:i+8]
+                keycode+=keypad_dict[key]
+            return keycode
+        except:
+            return 0
+
+
+    def reset(self):
+        self.bit_count = 0
+        self.data = 0
+
+    def process_scan(self):
+        bitLen = self.get_pending_bit_count()
+        if bitLen > 0 and bitLen <= 24: # changed 6/24/23 to help avoid spurious readings.
+            logging.debug("\n{}: BAD scan detected at {}: {}".format(datetime.now(), self.location, bitLen))
+            print("load dock bad scan: {}".format(bitLen))
+            self.reset() # fixed the triple scan problem
+            time.sleep(0.1)
+           
+        elif bitLen > 24:
+            data = "{:026b}".format(self.read_data())
+            print("\nscan detected at loading dock: " + str(data))
+            logging.debug("\n{}: scan detected at {}: {}".format(datetime.now(), self.location, bitLen))
+
+           # if last_scantime + timedelta(seconds = 3) < datetime.now(): 
+           #     last_scantime = datetime.now()
+           # else:
+           # continue
+
+            if unlock.isAllowed(session, self.location, data, data):
+                logging.debug("{}: Open Sesame".format(self.location))
+                GPIO.output(REAR_DOOR_RELAY_PIN, True)
+                GPIO.output(REAR_DOOR_LED_PIN, GPIO.LOW)
+                for i in range(0,8):
+                    GPIO.output(REAR_DOOR_SPKR_PIN, GPIO.LOW)
+                    time.sleep(.05)
+                    GPIO.output(REAR_DOOR_SPKR_PIN, GPIO.HIGH)
+                    time.sleep(.3)
+                GPIO.output(REAR_DOOR_LED_PIN, GPIO.HIGH)
+                GPIO.output(REAR_DOOR_RELAY_PIN, False)                       
+            else:
+                logging.debug("Loading Dock: Access denied")
+                GPIO.output(REAR_DOOR_SPKR_PIN, GPIO.LOW)
+                time.sleep(0.5)
+                GPIO.output(REAR_DOOR_SPKR_PIN, GPIO.HIGH)
+
+                
 if __name__ == "__main__":
    logging.debug("Starting")
    last_scantime = datetime.now()
@@ -186,8 +305,8 @@ if __name__ == "__main__":
    w = decoder(REAR_DOOR_A_PIN, REAR_DOOR_B_PIN)
    w2 = decoder(FRONT_DOOR_A_PIN, FRONT_DOOR_B_PIN)
    w3 = decoder(BSMITH_DOOR_A_PIN, BSMITH_DOOR_B_PIN)
+   w4 = KeypadDecoder(FOURTH_DOOR_A_PIN, FOURTH_DOOR_B_PIN)   
    logging.debug("READY")
-   loop_counter = 1
 
    while True:
        time.sleep(0.3) # without this, readings get reset to 0 too quickly I think.
@@ -300,6 +419,43 @@ if __name__ == "__main__":
                GPIO.output(BSMITH_DOOR_SPKR_PIN, GPIO.LOW)
                time.sleep(0.5)
                GPIO.output(BSMITH_DOOR_SPKR_PIN, GPIO.HIGH)
+
+
+
+       bitLen4 = w4.get_pending_bit_count()
+       if bitLen4 > 0 and bitLen4 <= 31: # changed 3/8/24 to help avoid spurious readings.
+           print("HERE", bitLen4)
+           # data = "{:026b}".format(w2.read_data())
+           logging.debug("\n" + str(datetime.now()) + ": BAD scan detected at lobby: " + str(bitLen3))
+           time.sleep(0.1)
+       elif bitLen4 > 31:
+           data = "{:032b}".format(w4.read_data())
+           logging.debug("\n" + str(datetime.now()) + ": scan detected at lobby: " + str(data))
+           if last_scantime + timedelta(seconds = 3) < datetime.now(): 
+               last_scantime = datetime.now()
+           else:
+               continue # end this iteration of the loop
+
+           print(w4.decode_data(data))
+
+           if unlock.isAllowed(session, "Lobby", data, data):               
+               logging.debug("Lobby Door: Open Sesame")
+               GPIO.output(BSMITH_DOOR_RELAY_PIN, True)
+               GPIO.output(BSMITH_DOOR_LED_PIN, GPIO.LOW)
+
+               for i in range(0,8):
+                   GPIO.output(BSMITH_DOOR_SPKR_PIN, GPIO.LOW)
+                   time.sleep(.05)
+                   GPIO.output(BSMITH_DOOR_SPKR_PIN, GPIO.HIGH)
+                   time.sleep(.3)
+
+               GPIO.output(BSMITH_DOOR_LED_PIN, GPIO.HIGH)
+               GPIO.output(BSMITH_DOOR_RELAY_PIN, False)
+           else:
+               logging.debug("Lobby Door: Access denied")
+               GPIO.output(BSMITH_DOOR_SPKR_PIN, GPIO.LOW)
+               time.sleep(0.5)
+               GPIO.output(BSMITH_DOOR_SPKR_PIN, GPIO.HIGH)               
 
                
 
