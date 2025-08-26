6684a41a90341d483bfc25d7dbd9ce02d2229d5d
Adam Spontarelli <adam@vector-space.org>
2025-08-26 11:23:39 -0400

    changed start_door script

--- start_door.sh
+++ start_door.sh
@@ -2,7 +2,6 @@
 # must be run as root
 
 echo "Starting door server"
-cd /home/vectorspace/RFID-Access/server
-python run.py &
+python /home/vectorspace/RFID-Access/server/run.py &
 python /home/vectorspace/RFID-Access/reader/MFRC522-python/read_wiegand.py
 echo "Finished"
